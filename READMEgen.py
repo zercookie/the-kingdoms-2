55d32738a5e6c3b17b548dbc3d6765f1cc31bcb5
zercookie <zercookie@gmail.com>
2025-10-02 11:18:43 +0100

    Create READMEgen.py

---
+++ READMEgen.py
@@ -0,0 +1,49 @@
+import tomllib
+import os
+
+def listFilesInDir(directory):
+    try:
+        items = os.listdir(directory)
+        files = [f for f in items if os.path.isfile(os.path.join(directory,f))]
+        return files
+    except FileNotFoundError:
+        print(f"Error: Directory '{directory}' not found.")
+        return []
+
+try:
+    mods = listFilesInDir("packwiz/mods/")
+    for mod in mods:
+        with open(f"packwiz/mods/{mod}","rb") as file:
+            data = tomllib.load(file)
+
+            main_keys = ["name","side","option"]
+            sub_keys = ["optional","default","description"]
+            result = {}
+
+            for key in main_keys:
+                if key in data:
+                    if type(data[key]) == str:
+                        result[key] = data[key]
+                    else:
+                        for sub in sub_keys:
+                            if sub in data[key]:
+                                result[sub] = data[key][sub]
+                else:
+                    for sub in sub_keys:
+                        result[sub] = None
+            print(result)
+
+except FileNotFoundError as e:
+    print("Error: File not found: ", e)
+except tomllib.TOMLDecodeError as e:
+    print("Error: Failed to parse TOML file: ", e)
+
+
+
+# | Name | Server | Client | Optional | Default | Description |
+# |------|--------|--------|----------|---------|-------------|
+#
+#
+#
+#
+#
