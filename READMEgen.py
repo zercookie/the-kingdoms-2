1aed1e5772a583778d9006fe62baa0e560f14fe8
zercookie <zercookie@gmail.com>
2025-10-03 11:23:14 +0100

    change tick and x markings

--- READMEgen.py
+++ READMEgen.py
@@ -1,6 +1,8 @@
 import tomllib
 import os
 
+modList = []
+
 def listFilesInDir(directory):
     try:
         items = os.listdir(directory)
@@ -9,9 +11,6 @@ def listFilesInDir(directory):
     except FileNotFoundError:
         print(f"Error: Directory '{directory}' not found.")
         return []
-
-modList = []
-
 
 def getModListInfoFromTOMLfiles():
     try:
@@ -30,14 +29,14 @@ def getModListInfoFromTOMLfiles():
                             if key == "side":
                                 match data[key]:
                                     case "client":
-                                        result["client"] = "✅"
-                                        result["server"] = "❌"
+                                        result["client"] = "✔"
+                                        result["server"] = "✖"
                                     case "server":
-                                        result["client"] = "❌"
-                                        result["server"] = "✅"
+                                        result["client"] = "✖"
+                                        result["server"] = "✔"
                                     case "both":
-                                        result["client"] = "✅"
-                                        result["server"] = "✅"
+                                        result["client"] = "✔"
+                                        result["server"] = "✔"
                             else:
                                 result[key] = data[key]
                         else:
@@ -45,14 +44,14 @@ def getModListInfoFromTOMLfiles():
                                 if sub in data[key]:
                                     match data[key][sub]:
                                         case True:
-                                            result[sub] = result[sub] = "✅"
+                                            result[sub] = result[sub] = "✔"
                                         case False:
-                                            result[sub] = result[sub] = "❌"
+                                            result[sub] = result[sub] = "✖"
                                         case _:
                                             result[sub] = data[key][sub]
                     else:
                         for sub in sub_keys:
-                            result[sub] = "❌"
+                            result[sub] = "✖"
                 modList.append(result)
     except FileNotFoundError as e:
         print("Error: File not found: ", e)
